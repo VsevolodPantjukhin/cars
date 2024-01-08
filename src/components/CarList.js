30c77cdbc33b800ab2697e2fcc701d127b36b8a6
haki_ainz <seva.pantiukhin@yandex.ru>
2024-01-08 16:33:14 +0400

    Listing Records

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,8 +5,34 @@ const CarList = () => {
     return state.cars.data;
   });
 
-  console.log(cars);
-  return <div>CarList</div>;
+  const handleCarDelete = (car) => {
+    //...
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          onClick={() => {
+            handleCarDelete(car);
+          }}
+          className="button is-danger"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 };
 
 export default CarList;
